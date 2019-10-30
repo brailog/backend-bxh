3e8519b98f92ed17114839a000566195dc14492d
Giovanni Veloso da Silva <gvs@ug3c37.windows.cin.ufpe.br>
2019-10-30 19:29:55 -0300

    Atualizando Project_hook.rb

--- cucumber/features/hooks/project_hook.rb
+++ cucumber/features/hooks/project_hook.rb
@@ -1,4 +1,18 @@
 Before '@project' do 
+    @name = Faker::Project.name
+    @description = Faker::Project.catch_phrase
+    @link1 = Faker::Project.catch_phrase
+    @link2 = Faker::Project.catch_phrase 
+    @category = Faker::Project.category
 
-    
+    @body = {
+            project: {
+                "name":@name,
+                "description":@description,
+                "link1":@link1,
+                "link2":@link2,
+                "category":@category
+            }               
+    }
+    @project = Project.new(@body)
 end
