da38a907277e093fbc9fc623f4b772147767099f
brailog <bielrammos@gmail.com>
2019-11-02 00:52:20 -0300

    Code style

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :update, :destroy]
+  before_action :set_project, only: %i[show update destroy]
 
   # GET /projects
   def index
-    # binding.pry
-    if params[:company_id] != nil
-      @projectsbycompanyid = Project.joins(:company).where(projects: {company_id: params[:company_id]})
+    if !params[:company_id].nil?
+      @projectsbycompanyid = Project.joins(:company).where(projects: { company_id: params[:company_id] })
       render json: @projectsbycompanyid
     else
-      
       @projects = Project.all
       render json: @projects
     end
   end
+
   # GET /projects/1
   def show
     render json: @project
@@ -44,13 +45,14 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def project_params
-      params.require(:project).permit(:name, :description, :link1, :link2, :category, :company_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def project_params
+    params.require(:project).permit(:name, :description, :link1, :link2, :category, :company_id)
+  end
 end
