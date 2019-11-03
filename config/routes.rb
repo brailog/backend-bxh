7a154d2831d3e6f96568be8da961f9b57900c069
brailog <bielrammos@gmail.com>
2019-11-02 22:05:54 -0300

    Criação de novas para o usuario

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   resources :projects
   resources :companies
   resources :users
+  
   resources :companies do 
     resources :projects 
+  end
+  resources :companies do
+    resources :users
+  end
+  resources :hunters do
+    resources :users
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
