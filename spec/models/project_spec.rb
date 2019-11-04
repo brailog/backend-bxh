d65fc12fb262aea57673e65764014904948af10b
brailog <bielrammos@gmail.com>
2019-11-04 16:27:33 -0300

    rspec test project with companyid broken

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Project, :type => :model do    
+  it "is valid with all valid attributes" do
+    @name = Faker::Game.title
+    @description = Faker::Game.genre
+    @link1 = "www."+Faker::DcComics.hero+".com"
+    @link2 = "www."+Faker::DcComics.villain+".com"
+    @category = Faker::Music.genre
+    binding.pry
+    project = Project.new(
+        {
+                "name":@name,
+                "description":@description,
+                "link1":@link1,
+                "link2":@link2,
+                "category":@category,
+                "company_id":100
+        }
+)
+    expect(project).to be_valid
+  end
+end
