4d990f2ddbad424f1b16198b2d43fb1b4d14af3b
brailog <bielrammos@gmail.com>
2019-11-04 14:11:29 -0300

    start validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
+    validates :password, confirmation: true, unless: -> { password.blank? }
+    validates :name, presence: true
+    validates :description, presence: true
     has_many :projects
 end
