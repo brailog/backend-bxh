871a3d7940d600ca8d70edcf1a2f25ca4d7f0bd9
brailog <bielrammos@gmail.com>
2019-11-08 18:53:29 -0300

    ATE QUEM FIM O TESTE FOI!!!

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,22 +1,22 @@
-
-# FactoryBot.define do
-#     factory :project do
-#             name {"Heller LLC"} 
-#             description {"Synergistic next generation matrices"}
-#             cnpj {"31171156000754"}
-#             phone {"(346) 103-3387 x790"}
-#             email { "nathan@mcdermott.net"}
-#             password {"12345678"} 
-#     end
-# end
+company = Company.new(
+    {
+    "name":"Heller LLC",
+    "description":"Synergistic next generation matrices",
+    "cnpj":"31171156000754",
+    "phone":"(346) 103-3387 x790",
+    "email":"nathan@mcdermott.net",
+    "password":"12345678"
+    }
+)
+company.save!
 
 FactoryGirl.define do
     factory :project do
-        name { "Heller LLC" }
-        description { "Synergistic next generation matrices" }
-        cnpj { "31171156000754" }
-        phone { "(346) 103-3387 x790" }
-        email { "nathan@mcdermott.net" }
-        password { "12345678" }
-    end
+        name { "Nome" }
+        description { "description" }
+        link1 { "link1" }
+        link2 { "link2" }
+        category { "category" }
+        company_id { company.id }
+    end 
 end
