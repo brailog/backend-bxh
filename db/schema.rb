6ab226368b26628d5af17c19d7d6fc2c6238a295
brailog <bielrammos@gmail.com>
2019-11-20 02:03:31 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_233355) do
+ActiveRecord::Schema.define(version: 2019_11_20_024744) do
+
+  create_table "bug_requests", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "repository_link"
+    t.string "live_link"
+    t.string "status"
+    t.integer "project_id", null: false
+    t.integer "hunter_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hunter_id"], name: "index_bug_requests_on_hunter_id"
+    t.index ["project_id"], name: "index_bug_requests_on_project_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -63,6 +77,15 @@ ActiveRecord::Schema.define(version: 2019_11_16_233355) do
     t.index ["company_id"], name: "index_projects_on_company_id"
   end
 
+  create_table "rewards", force: :cascade do |t|
+    t.string "type"
+    t.string "value"
+    t.integer "bugrequest_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bugrequest_id"], name: "index_rewards_on_bugrequest_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -74,7 +97,10 @@ ActiveRecord::Schema.define(version: 2019_11_16_233355) do
     t.index ["hunter_id"], name: "index_users_on_hunter_id"
   end
 
+  add_foreign_key "bug_requests", "hunters"
+  add_foreign_key "bug_requests", "projects"
   add_foreign_key "projects", "companies"
+  add_foreign_key "rewards", "bugrequests"
   add_foreign_key "users", "companies"
   add_foreign_key "users", "hunters"
 end
