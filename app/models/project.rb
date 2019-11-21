417bb73981d602dbb5a3922e6dc7b32d190e67c7
brailog <bielrammos@gmail.com>
2019-11-20 22:23:36 -0300

    project validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :link1, presence: true
+  validates :category, presence: true
+
+  validates :name, length: { minimum: 3 }
+  validates :description, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed" }
+
   belongs_to :company, class_name: 'Company', foreign_key: 'company_id'
   has_many :bugrequests, class_name: 'BugRequest', foreign_key: 'bugrequest_id'
 end
